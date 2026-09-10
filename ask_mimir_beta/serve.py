729c5da478712521079fdebdc1c50fded16cce09
thomasjwpetterson-star <thomas.j.w.petterson@gmail.com>
2026-09-10 16:12:04 +1200

    Trigger Ask Mimir release deployment

--- ask_mimir_beta/serve.py
+++ ask_mimir_beta/serve.py
@@ -10,6 +10,7 @@ from bootstrap_data import bootstrap
 
 
 if __name__ == "__main__":
+    # Resolve the current atomic manifest before importing the API runtime.
     bootstrap()
     os.environ["ASK_MIMIR_ALLOW_TEST_IDENTITIES"] = "0"
     os.environ.setdefault("ASK_MIMIR_STRICT_CITATIONS", "1")
